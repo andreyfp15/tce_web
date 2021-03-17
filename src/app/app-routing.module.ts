9f053b80f70bf642b13e791c8bb25c4cf721293e
Andrey Felipe Pereira <andreyfp15@gmail.com>
2021-03-17 00:13:52 -0300

    create categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdmCategoryComponent } from './adm-category/adm-category.component';
 import { AdmTextComponent } from './adm-text/adm-text.component';
-
+import { AdmUserComponent } from './adm-user/adm-user.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component'
 
 
 const routes: Routes = [
-  { path: '', component: AdmTextComponent },
+  { path: '', component: CategoryComponent },
   //{ path: 'login', component: LoginComponent },
-  { path: 'admcategory', component: AdmCategoryComponent },
-  { path : 'admtext', component: AdmTextComponent }
+  { path : 'admcategory', component: AdmCategoryComponent },
+  { path : 'admtext', component: AdmTextComponent },
+  { path : 'admuser', component : AdmUserComponent },
+  { path : 'home', component : HomeComponent },
+  { path : 'category', component : CategoryComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AdmCategoryComponent } from './adm-category/adm-category.component';
-import { AdmTextComponent } from './adm-text/adm-text.component';
-
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AdmCategoryComponent } from './adm-category/adm-category.component';
+import { AdmTextComponent } from './adm-text/adm-text.component';
+import { AdmUserComponent } from './adm-user/adm-user.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AdmCategoryComponent,
-    AdmTextComponent
+    AdmTextComponent,
+    AdmUserComponent,
+    HomeComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
